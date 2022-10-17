78a76975b74297e33133ff12841696479f81c370
sluzerpp <bistboy@list.ru>
2022-10-17 14:04:25 +0300

    feat: complete encode-line task

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  if (str == "") return ""
+  let c = 0
+  let newStr = ""
+  let current = ""
+  current = str[0]
+  for(let i =0; i < str.length; i++) {
+    if (current != str[i]) {
+      newStr += (c==1 ? "" : c) + current
+      current = str[i]
+      c = 1
+    } else {
+      c++
+    }
+  }
+  newStr += (c==1 ? "" : c) + current
+  return newStr
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (rev=true) {
+    this.rev = rev
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(text, key) {
+    if (text == undefined || key == undefined) throw new Error("Incorrect arguments!")
+    if (!text || !key) throw new Error("Incorrect arguments!") 
+    let l = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let c = 0
+    text = text.toUpperCase()
+    let square = this.getSquare()
+    let s = "";
+    key = key.toUpperCase()
+    console.log(square)
+    for (let i = 0; i < text.length; i++) {
+      if (l.includes(text[i])) {
+        s += square[l.indexOf(text[i])][l.indexOf(key[(i-c) % key.length])];
+      } else {
+        s += text[i]
+        c++
+      }
+    }
+    if (!this.rev) {
+      return s.split().reverse().join()
+    }
+    return s;
   }
+  decrypt(cipher,key) {
+    if (cipher == undefined || key == undefined) throw new Error("Incorrect arguments!")
+    if (!cipher || !key) throw new Error("Incorrect arguments!") 
+    key = key.toUpperCase()
+    cipher = cipher.toUpperCase()
+    if (!this.rev) {
+      cipher = cipher.split().reverse().join()
+    }
+    let square = this.getSquare()
+    let l = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let s = "";
+    let coll
+    let c = 0
+    for (let i = 0; i < cipher.length; i++) {
+      if (l.includes(cipher[i])) {
+        let row = l.indexOf(key[(i-c) % key.length])
+            coll = square[row].indexOf(cipher[i]);
+        s += l[coll];
+      } else {
+        s += cipher[i]
+        c++
+      }
+    }
+    if (!this.rev) {
+      return s.split().reverse().join()
+    }
+    return s;
+  }
+
+  getSquare () {
+    let l = "ABCDEFGHIJKLMNOPQRSTUVWXYZ", square = [];
+    for (let i = 0; i < l.length; i++) {
+        square[i] = l.slice(i).concat(l.slice(0, i));
+    }
+    return square
+  }
+
 }
 
 module.exports = {
